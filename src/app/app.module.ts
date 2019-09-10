9bbd67a87f842b39d19c0de313377ea126808495
drood <dbernstein1987@hotmail.com>
2019-09-10 14:11:02 +0200

    create login compoment and label/input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InputLabelComponent } from './login/input-label/input-label.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InputLabelComponent
   ],
   imports: [
     BrowserModule,
