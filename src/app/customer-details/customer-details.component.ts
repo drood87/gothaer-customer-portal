18ee02751b50299ef9a3cd54dc72299178cf6e2c
drood <dbernstein1987@hotmail.com>
2019-09-11 18:17:18 +0200

    local storage now working

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -17,24 +17,50 @@ export class CustomerDetailsComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    /*check if customer has insurances, if not replace with string*/
-    const insurances = (function() {
-      if (history.state.data[0].selected_insurances.length === 0) {
-        return 'No insurances';
-      } else {
-        return history.state.data[0].selected_insurances;
-      }
-    })();
+    /*check if it's first time loading it and if there is a history from route changing and set object then otherwise retrieve data from local storage*/
+    if (history.state.data) {
+      /*check if customer has insurances, if not replace with string*/
+      const insurances = (function() {
+        if (history.state.data[0].selected_insurances.length === 0) {
+          return ['No insurances'];
+        } else {
+          return history.state.data[0].selected_insurances;
+        }
+      })();
 
-    this.customer = {
-      name: history.state.data[0].name,
-      membership_type: history.state.data[0].membership_type,
-      age: history.state.data[0].age,
-      selected_insurances: insurances.map(insurance =>
-        insurance.replace('_', ' ')
-      )
-    };
+      this.customer = {
+        name: history.state.data[0].name,
+        membership_type: history.state.data[0].membership_type,
+        age: history.state.data[0].age,
+        selected_insurances: insurances.map(insurance =>
+          insurance.replace('_', ' ')
+        )
+      };
+      /*store customer object into local storage to persist data on page reload (just a little hack as I'm not saving it on a database otherwise we could make an HTTP request to fetch the data of course) */
+      localStorage.setItem('customerData', JSON.stringify(this.customer));
+    } else if (!history.state.data) {
+      const customerData = (function() {
+        const retrieveData = localStorage.getItem('customerData');
+        const data = JSON.parse(retrieveData);
+        return data;
+      })();
 
-    console.log(this.customer);
+      const insurances = (function() {
+        if (customerData.selected_insurances.length === 0) {
+          return ['No insurances'];
+        } else {
+          return customerData.selected_insurances;
+        }
+      })();
+
+      this.customer = {
+        name: customerData.name,
+        membership_type: customerData.membership_type,
+        age: customerData.age,
+        selected_insurances: insurances.map(insurance =>
+          insurance.replace('_', ' ')
+        )
+      };
+    }
   }
 }
