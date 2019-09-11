1b2d16123439530f23140296ff4fc04564cab812
drood <dbernstein1987@hotmail.com>
2019-09-11 16:38:08 +0200

    autocomplete feature added

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import undefined = require('firebase/empty-import');
 
 @Component({
   selector: 'app-customer-details',
@@ -17,11 +18,21 @@ export class CustomerDetailsComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    const insurances = history.state.data[0].selected_insurances.map(
+      insurance => {
+        if (insurance === undefined) {
+          return console.log('empty');
+        } else {
+          return insurance;
+        }
+      }
+    );
+
     this.customer = {
       name: history.state.data[0].name,
       membership_type: history.state.data[0].membership_type,
       age: history.state.data[0].age,
-      selected_insurances: history.state.data[0].selected_insurances
+      selected_insurances: insurances
     };
 
     console.log(this.customer);
