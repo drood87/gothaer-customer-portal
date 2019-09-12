ee7cd9b2211776879d03b6f115b5d07c7bd8d73d
drood <dbernstein1987@hotmail.com>
2019-09-12 10:29:27 +0200

    save products in const

--- src/app/customer-details/insurances/insurances.component.ts
+++ src/app/customer-details/insurances/insurances.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { stringToKeyValue } from '@angular/flex-layout/extended/typings/style/style-transforms';
 
 @Component({
   selector: 'app-insurances',
@@ -8,6 +9,21 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./insurances.component.scss']
 })
 export class InsurancesComponent implements OnInit {
+  /* fetch data for the customer from parent element */
+  @Input() customerData: {
+    name: string;
+    age: number;
+    membership_type: string;
+    selected_insurances: any;
+  };
+
+  public products: {
+    accidentInsurance: object;
+    lifeInsurance: object;
+    stickers: object;
+    carInsurance: object;
+  };
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
@@ -15,13 +31,21 @@ export class InsurancesComponent implements OnInit {
   }
 
   fetchProducts() {
-    this.http
+    return this.http
       .get('https://api.jsonbin.io/b/5d7622dfc22e9d0afd2958a7', {
         headers: new HttpHeaders({
           'secret-key':
             '$2a$10$2ffWVgeCpYtciy0QzJ7fuOH5rIzs90CLGdFetjWCiTnbqp5/t73mi'
         })
       })
-      .subscribe(response => console.log(response));
+      .subscribe(response => {
+        this.products = {
+          accidentInsurance: response['accident_insurance'],
+          lifeInsurance: response['life_insurance'],
+          stickers: response['gothaer_stickers'],
+          carInsurance: response['car_insurance']
+        };
+        console.log(this.products);
+      });
   }
 }
