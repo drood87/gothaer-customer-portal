117766eb72bcfe445743cd2dda70e2ea02d8cec4
drood <dbernstein1987@hotmail.com>
2019-09-13 19:41:55 +0200

    preparing auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'customer-details', component: CustomerDetailsComponent }
+  {
+    path: 'customer-details',
+    component: CustomerDetailsComponent,
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
